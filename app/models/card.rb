c3b5c18a11fd2a93ea05fe2f67e0084b9c0cf581
Naoyuki Ishida <39.ishida@gmail.com>
2016-02-24 07:53:37 +0000

    apply bootstrap

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,8 @@
 class Card < ActiveRecord::Base
+  after_initialize do
+    self.start = "2000-01-01 09:30:00"
+    self.end = "2000-01-01 18:30:00"
+    self.reststart = "2000-01-01 12:00:00"
+    self.restend = "2000-01-01 13:00:00"
+  end
 end
