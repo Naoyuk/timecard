c4a1be4a0bbf3aeef1a921bce8f02d87f9661f44
Naoyuki Ishida <39.ishida@gmail.com>
2016-02-26 16:31:32 +0000

    not use sendgrid, and use gmail again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,11 +66,11 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'timecard-hem.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => 'smtp.sendgrid.net',
+    :address => 'smtp.gmail.com',
     :port => 587,
-    :domain => 'heroku.com',
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'gmail.com',
+    :user_name => ENV['MAIL_USER'],
+    :password => ENV['MAIL_PASS'],
     :authentication => :plain,
     :enable_starttls_auto => true
   }
