033238afb160ed172be38178bd6a1946bb013f22
Naoyuki Ishida <39.ishida@gmail.com>
2016-02-26 16:06:30 +0000

    send mail from sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,11 +66,11 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'timecard-hem.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => 'smtp.gmail.com',
+    :address => 'smtp.sendgrid.net',
     :port => 587,
-    :domain => 'gmail.com',
-    :user_name => ENV['MAIL_USER'],
-    :password => ENV['MAIL_PASS'],
+    :domain => 'heroku.com',
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
     :authentication => :plain,
     :enable_starttls_auto => true
   }
