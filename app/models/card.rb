48994a225760b5c026a80a617db22a3eb620fcec
Naoyuki Ishida <39.ishida@gmail.com>
2016-03-01 08:07:29 +0000

    turn card's model on and modify its code

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,13 +1,13 @@
 class Card < ActiveRecord::Base
-  # after_initialize :set_default, if: :new_record?
-  # 
-  # private
-  # def set_default 
-  #   d = Time.now
-  #   self.date = Date.today
-  #   self.start = Time.local(d.year, d.month, d.day, 9, 30, 0)
-  #   self.end = Time.local(d.year, d.month, d.day, 18, 30, 0)
-  #   self.reststart = Time.local(d.year, d.month, d.day, 12, 0, 0)
-  #   self.restend = Time.local(d.year, d.month, d.day, 13, 0, 0)
-  # end
+  after_initialize :set_default, if: :new_record?
+  
+  private
+  def set_default 
+    d = Time.now
+    self.date ||= Date.today
+    self.start ||= Time.local(d.year, d.month, d.day, 9, 30, 0)
+    self.end ||= Time.local(d.year, d.month, d.day, 18, 30, 0)
+    self.reststart ||= Time.local(d.year, d.month, d.day, 12, 0, 0)
+    self.restend ||= Time.local(d.year, d.month, d.day, 13, 0, 0)
+  end
 end
