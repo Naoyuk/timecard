51b0e12f6c23ece4c39219f06a19fba8e8e6c56e
Naoyuki Ishida <39.ishida@gmail.com>
2021-01-28 23:33:20 -0800

    deligate all mail environments to .env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,9 +66,9 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'timecard-hem.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => 'smtp.gmail.com',
-    :port => 587,
-    :domain => 'gmail.com',
+    :address => ENV['MAIL_SMTP'],
+    :port => ENV['MAIL_PORT'],
+    :domain => ENV['MAIL_DOMAIN'],
     :user_name => ENV['MAIL_USER'],
     :password => ENV['MAIL_PASS'],
     :authentication => :plain,
